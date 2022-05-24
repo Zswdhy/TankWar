6f222bb6ca520db4649fdc456059e13723b7fa0e
peiqing.guo <15225967756@sina.cn>
2022-05-24 16:49:01 +0800

    feat:使用线程重画坦克的位置【动起来的效果】

--- src/com/iston/tankwarclient/TankWarClient.java
+++ src/com/iston/tankwarclient/TankWarClient.java
@@ -6,12 +6,16 @@ import java.awt.event.WindowEvent;
 
 
 public class TankWarClient extends Frame {
+
+    int x = 50, y = 50;
+
     // 窗口重画的时候，自动调用paint()方法
     public void paint(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.RED); //设置画笔颜色
-        g.fillOval(50, 50, 30, 30); //设置坦克的初始位置
+        g.fillOval(x, y, 30, 30); //设置坦克的初始位置
         g.setColor(c); // 恢复画笔颜色
+        x += 3;
     }
 
     public void launchFrame() {
@@ -26,11 +30,27 @@ public class TankWarClient extends Frame {
         }); //匿名类，重写内部方法
         this.setResizable(false); // 禁止修改窗体的大小
         setVisible(true);// 显示窗体
+        new Thread(new paintThread()).start();
     }
 
     public static void main(String[] args) {
         System.out.println("test");
         TankWarClient tc = new TankWarClient();
         tc.launchFrame();
+    }
+
+
+    private class paintThread implements Runnable {
+
+        public void run() {
+            while (true) {
+                repaint(); // 调用外部类的重画方法，因为未重写repaint，即调用父类Frame的repaint()方法
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
