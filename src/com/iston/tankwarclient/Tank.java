df9c8cb8c08f11ed670e13ab98b5c55fa2bf78ef
peiqing.guo <15225967756@sina.cn>
2022-05-26 15:48:27 +0800

    feat:坦克8个方向的移动

--- src/com/iston/tankwarclient/Tank.java
+++ src/com/iston/tankwarclient/Tank.java
@@ -5,8 +5,17 @@ import java.awt.event.KeyEvent;
 
 public class Tank {
 
-    public static final int TANK_SPEED = 5;
-    int x, y;//位置坐标
+    public static final int TANK_X_SPEED = 5;
+    public static final int TANK_Y_SPEED = 5;
+    private int x, y;//位置坐标
+
+    private boolean left = false, up = false, right = false, down = false;
+
+    enum Direction {
+        L, LU, U, RU, R, RD, D, LD, STOP
+    }
+
+    private Direction dir = Direction.STOP;
 
     // 同名的构造方法
     public Tank(int x, int y) {
@@ -14,20 +23,69 @@ public class Tank {
         this.y = y;
     }
 
+    void move() {
+        switch (dir) {
+            case L -> x -= TANK_X_SPEED;
+            case LU -> {
+                x -= TANK_X_SPEED;
+                y -= TANK_Y_SPEED;
+            }
+            case U -> y -= TANK_Y_SPEED;
+            case RU -> {
+                x += TANK_X_SPEED;
+                y -= TANK_Y_SPEED;
+            }
+            case R -> x += TANK_X_SPEED;
+            case RD -> {
+                x += TANK_X_SPEED;
+                y += TANK_Y_SPEED;
+            }
+            case D -> y += TANK_Y_SPEED;
+            case LD -> {
+                x -= TANK_X_SPEED;
+                y += TANK_Y_SPEED;
+            }
+        }
+    }
+
     public void draw(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.RED); //设置画笔颜色
         g.fillOval(x, y, 30, 30); //设置坦克的初始位置
         g.setColor(c); // 恢复画笔颜色
+        move();
     }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_LEFT -> x -= TANK_SPEED;
-            case KeyEvent.VK_UP -> y -= TANK_SPEED;
-            case KeyEvent.VK_RIGHT -> x += TANK_SPEED;
-            case KeyEvent.VK_DOWN -> y += TANK_SPEED;
+            case KeyEvent.VK_LEFT -> left = true;
+            case KeyEvent.VK_UP -> up = true;
+            case KeyEvent.VK_RIGHT -> right = true;
+            case KeyEvent.VK_DOWN -> down = true;
+        }
+        locateDirection();
+    }
+
+    void locateDirection() {
+        if (left && !up && !right && !down) {
+            dir = Direction.L;
+        } else if (left && up && !right && !down) {
+            dir = Direction.LU;
+        } else if (!left && up && !right && !down) {
+            dir = Direction.U;
+        } else if (!left && up && right && !down) {
+            dir = Direction.RU;
+        } else if (!left && !up && right && !down) {
+            dir = Direction.R;
+        } else if (!left && !up && right && down) {
+            dir = Direction.RD;
+        } else if (!left && !up && !right && down) {
+            dir = Direction.D;
+        } else if (left && !up && !right && down) {
+            dir = Direction.LD;
+        } else if (!left && !up && !right && !down) {
+            dir = Direction.STOP;
         }
     }
 }
