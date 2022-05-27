7cd90e4e7665a752b134688e20bbf29aaded9db3
peiqing.guo <15225967756@sina.cn>
2022-05-27 11:29:32 +0800

    feat：在坦克上画炮筒，方向同步

--- src/com/iston/tankwarclient/Tank.java
+++ src/com/iston/tankwarclient/Tank.java
@@ -20,7 +20,8 @@ public class Tank {
         L, LU, U, RU, R, RD, D, LD, STOP
     }
 
-    private Direction dir = Direction.STOP;
+    private Direction dir = Direction.STOP; // 坦克方向
+    private Direction gunDir = Direction.D; //炮筒方向
 
     // 同名的构造方法
     public Tank(int x, int y) {
@@ -56,6 +57,10 @@ public class Tank {
                 y += TANK_Y_SPEED;
             }
         }
+        // 坦克方向和炮筒方向同时更新
+        if (this.dir != Direction.STOP) {
+            this.gunDir = this.dir;
+        }
     }
 
     public void draw(Graphics g) {
@@ -63,6 +68,34 @@ public class Tank {
         g.setColor(Color.RED); //设置画笔颜色
         g.fillOval(x, y, TANK_WIDTH, TANK_HIGH); //设置坦克的初始位置
         g.setColor(c); // 恢复画笔颜色
+        // 根据坦克的方向画炮筒
+        switch (gunDir) {
+            case L -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x, y + TANK_HIGH / 2);
+            }
+            case LU -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x, y);
+            }
+            case U -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x + TANK_WIDTH / 2, y);
+            }
+            case RU -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x + TANK_WIDTH, y);
+            }
+            case R -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x + TANK_WIDTH, y + TANK_HIGH / 2);
+            }
+            case RD -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x + TANK_WIDTH, y + TANK_HIGH);
+            }
+            case D -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x + TANK_WIDTH / 2, y + TANK_HIGH);
+            }
+            case LD -> {
+                g.drawLine(x + TANK_WIDTH / 2, y + TANK_HIGH / 2, x, y + TANK_HIGH);
+            }
+        }
+
         move();
     }
 
@@ -116,7 +149,7 @@ public class Tank {
     public Missile fire() {
         int x = this.x + Tank.TANK_WIDTH / 2 - Missile.MISSILE_WIDTH / 2;
         int y = this.y + Tank.TANK_HIGH / 2 - Missile.MISSILE_HIGH / 2;
-        Missile missile = new Missile(x, y, dir);
+        Missile missile = new Missile(x, y, gunDir); // 根据炮筒的方向fire
         return missile;
     }
 }
